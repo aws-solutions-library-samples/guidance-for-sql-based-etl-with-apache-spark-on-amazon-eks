782e5cd2d2666688cd20544dd5c3dc0800ce1789
Melody <meloyang@amazon.com>
2024-01-31 22:42:01 +1100

    change key in wordcount.py

--- deployment/app_code/job/wordcount.py
+++ deployment/app_code/job/wordcount.py
@@ -2,5 +2,5 @@ import sys
 from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName('NYC taxi vendor count').getOrCreate()
 df = spark.read.option("header",True).csv(sys.argv[1])
-df.filter(df["VendorID"].isNotNull()).select("VendorID").groupBy("VendorID").count().write.mode("overwrite").parquet(sys.argv[2])
+df.filter(df["vendor_name"].isNotNull()).select("VendorID").groupBy("VendorID").count().write.mode("overwrite").parquet(sys.argv[2])
 exit()
