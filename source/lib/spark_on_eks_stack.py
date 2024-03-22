28ba55d8b8e1a54ee7be83ba78d72d112555937e
Melody <meloyang@amazon.com>
2024-03-22 18:19:56 +1100

    change license from apache2.0 to MIT-0

--- source/lib/spark_on_eks_stack.py
+++ source/lib/spark_on_eks_stack.py
@@ -1,16 +1,3 @@
-######################################################################################################################
-# Copyright 2020-2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.                                      #
-#                                                                                                                   #
-# Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance    #
-# with the License. A copy of the License is located at                                                             #
-#                                                                                                                   #
-#     http://www.apache.org/licenses/LICENSE-2.0                                                                    #
-#                                                                                                                   #
-# or in the 'license' file accompanying this file. This file is distributed on an 'AS IS' BASIS, WITHOUT WARRANTIES #
-# OR CONDITIONS OF ANY KIND, express o#implied. See the License for the specific language governing permissions     #
-# and limitations under the License.  																				#                                                                              #
-######################################################################################################################
-
 from aws_cdk import (Stack, CfnOutput, Duration, RemovalPolicy, Aws, Fn, CfnParameter, aws_eks as eks,aws_secretsmanager as secmger,aws_kms as kms)
 from constructs import Construct
 from lib.cdk_infra.network_sg import NetworkSgConst
